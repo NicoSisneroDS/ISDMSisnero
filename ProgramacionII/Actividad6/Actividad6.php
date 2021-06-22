bda8443e6bc4e478493e4c92add66a87a09df2fc
NicolasSisneroISDM <nico.sisneroks@gmail.com>
2021-06-22 01:14:40 -0300

    correcciones finales

--- ProgramacionII/Actividad6/Actividad6.php
+++ ProgramacionII/Actividad6/Actividad6.php
@@ -13,7 +13,8 @@
     <h3>Ingrese el numero de filas y columnas para generar la tabla.</h3>
         Filas  <input type="number" name="nFila" id="idFila"></br></br>
         Columnas  <input type="number" name="nColumna" id="idColumna"></br></br>
-        Dato (Opcional)  <input type="text" name="nDato" id="idDato">
+        <h4>OPCIONAL</h4>
+        Dato  <input type="text" name="nDato" id="idDato">
         en fila  <input type="number" name="nFila1" id="idFila1">
         y columna  <input type="number" name="nColumna1" id="idColumna1"></br></br>
         <input type="submit" name="nSubmit" id="idSubmit" value="Generar">
@@ -32,32 +33,62 @@
                 $this->columnas = $nrocolumnas;
             }
 
-            function generaciontabla($filas,$columnas,$d,$f,$c){
-                if($filas <= 0 || $columnas <= 0){
+            function generaciontabla($d,$fi,$co){
+                if($this->filas <= 0 || $this->columnas <= 0){
                     echo '</br> Error. No es posible generar la tabla ya que puso un numero negativo, cero o está vacio.';
                 }else{
-                    echo '</br><table border=2>';
-                    for ($fila=1; $fila <= $filas; $fila++) { 
-                        echo '<tr>';
-                        for ($columna=1; $columna <= $columnas; $columna++) { 
-                            if($fila == $f && $columna == $c){
-                                echo '<td>'.$d;
+                    $this->comienzoTabla();
+                    $t = 1;
+                    for ($f=1; $f <= $this->filas; $f++) { 
+                        $this->comienzoFila();
+                        for ($c=1; $c <= $this->columnas; $c++){
+                            if($fi == $f && $co == $c){
+                                $this->comienzoColumna();
+                            	echo $d;
+                                $t++;
                             }else{
-                                $texto = '<td> Fila '. $fila.', Columna '.$columna;
-                                echo $texto;
-                                }
+                                $this->comienzoColumna();
+                                echo $t;
+                                $t++;
                             }
-                        echo '</tr>';
+                            $this->cierreColumna();
+                            }
+                        $this->cierreFila();
                         }
-                    echo '</table>';
+                    $this->cierreTabla();
                 }
             }
+
+            function comienzoTabla(){
+                echo '</br><table border=2>';
+            }
+
+            function cierreTabla(){
+                echo '</table>';
+            }
+
+            function comienzoFila(){
+                echo '<tr>';
+            }
+
+            function cierreFila(){
+                echo '</tr>';
+            }
+            
+            function comienzoColumna(){
+                echo '<td>';
+            }
+            
+            function cierreColumna(){
+                echo '</td>';
+            }
+
         }
 
         //INSTANCIA
         if(isset($_REQUEST['nSubmit'])){
             $tabla = new Tabla($_REQUEST['nFila'],$_REQUEST['nColumna']);
-            echo $tabla->generaciontabla($_REQUEST['nFila'],$_REQUEST['nColumna'],$_REQUEST['nDato'],$_REQUEST['nFila1'],$_REQUEST['nColumna1']);
+            $tabla->generaciontabla($_REQUEST['nDato'],$_REQUEST['nFila1'],$_REQUEST['nColumna1']);
         }
     ?>
 
