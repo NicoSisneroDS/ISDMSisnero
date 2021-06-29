5f171a86fbe356206877ff0e3bc59bf19e6a14e3
NicolasSisneroISDM <nico.sisneroks@gmail.com>
2021-06-29 16:25:15 -0300

    Finalizacion act 7

--- ProgramacionII/Actividad7/Act7.php
+++ ProgramacionII/Actividad7/Act7.php
@@ -1,39 +1,87 @@
 <?php
+    echo 'PARTE 1';
     class cabecera{
         //ATRIBUTOS
         public $titulo;
         //METODOS
         public function __construct($t){
-            $this->titulo = '<h1 style="float: left;">'.$t.'</h1></br>';
+            $this->titulo = $t;
         }
+        public function printTitulo(){
+            echo '<h1 align="left">'.$this->titulo.'</h1></br>';
+        }
+    }
 
-        public function inicioprint(){
-            echo '<!DOCTYPE html>
-            <html lang="es">
-            <head>
-                <meta charset="UTF-8">
-                <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Actividad 7 - Parte 1</title>
-            </head>
-            <body>';
+    class cuerpo{
+        //ATRIBUTOS
+        public $texto = array();
+        //METODOS
+        public function insertarParrafo($valor){
+            $this->texto[] = $valor.'</br>';
         }
-        
-        public function printmensaje(){
-            return $this->titulo;
+        public function printParrafo(){
+            for($f = 0;$f < count($this->texto);$f++){
+                echo '<p>'.$this->texto[$f].'</p>';
+            }
         }
-        
-        public function finprint(){
-            echo '
-            </body>
-            </html>';
-        }
+    }
 
+
+    class pie{
+        //ATRIBUTOS
+        public $pie;
+        //METODOS
+        public function __construct($p){
+            $this->pie = $p;
+        }
+        public function printPie(){
+            echo '<h4 align="center">'.$this->pie.'</h4></br>';
+        }
     }
 
     $cab = new cabecera('Este es un titulo');
-    echo $cab->inicioprint();
-    echo $cab->printmensaje();
-    echo $cab->finprint();
+        $cab->printTitulo();
+    
+    $parr = new cuerpo();
+        $parr->insertarParrafo('PARRAFO 1');
+        $parr->insertarParrafo('parrafo 2');
+        $parr->insertarParrafo('ParrafO 3');
+        $parr->printParrafo();
 
+    $pie = new pie('Este es un pie de pagina');
+        $pie->printPie();
+    
+    echo '</br></br></br>';
+    echo 'PARTE 2';
+    echo '</br></br></br>';
+
+    class pagina{
+        //ATRIBUTOS
+        public $Cabecera;
+        public $Cuerpo;
+        public $Pie;
+        //METODOS
+        public function __construct($ca,$pi){
+            $this->Cabecera = new cabecera($ca);
+            $this->Cuerpo = new cuerpo();
+            $this->Pie = new pie($pi);
+        }
+
+        public function insertarCuerpo($c){
+            $this->Cuerpo->insertarParrafo($c).'</br>';
+        }
+
+        public function graficar(){
+            echo $this->Cabecera->printTitulo();
+            echo $this->Cuerpo->printParrafo();
+            echo $this->Pie->printPie();
+        }
+    }
+
+    $parte2 = new pagina('Este es un titulo','Este es un pie de pagina');
+        $parte2->insertarCuerpo('PARRAFO 1');
+        $parte2->insertarCuerpo('parrafo 2');
+        $parte2->insertarCuerpo('ParrafO 3');
+        $parte2->graficar();
+    
 ?>
